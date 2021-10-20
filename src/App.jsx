4784e5477ed4d47c40f41b882b5d64e85763da6f
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-19 22:11:17 -0300

    desenvolvido componente MessageContainer para mostrar a menssagem de acondo com o state message do redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import MessageContainer from './components/MessageContainer';
 import MainRouters from './routers/MainRouters';
 
 function App() {
   return (
     <div className="App">
+      <MessageContainer />
       <MainRouters />
     </div>
   );
--- src/components/MessageContainer/index.jsx
+++ src/components/MessageContainer/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionMessage } from '../../redux/actions';
 import './style.css';
 
-const MessageContainer = () => (
-  <h1>MessageContainer</h1>
-);
+const MessageContainer = () => {
+  const message = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+
+  const handleClick = () => { dispatch(actionMessage('')); };
+
+  if (!message) return '';
+
+  return (
+    <div className="MessageContainer">
+      <p>{message}</p>
+      <button type="button" onClick={handleClick}>OK</button>
+    </div>
+  );
+};
 
 export default MessageContainer;
