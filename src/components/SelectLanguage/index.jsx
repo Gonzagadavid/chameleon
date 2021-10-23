b8aeb8f1564d40fe4e17da9535d1e200e1542d3c
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-23 19:00:10 -0300

    instalado componete library de select para poder renderizar a bandeira do idoma a ser selecionado, adicionado no componente SelectLanguage, refatorando de forma mostrar a bandeira, iniciado estilização do mesmo

--- src/components/SelectLanguage/index.jsx
+++ src/components/SelectLanguage/index.jsx
@@ -2,15 +2,60 @@ import {
   func, objectOf, string,
 } from 'prop-types';
 import React from 'react';
+import Select from 'react-select';
 import arrayKeys from '../../functions/arrayKeys';
+import './style.css';
 
 const SelectLanguage = ({ setLanguage, objectItems, keyOption }) => {
-  const options = arrayKeys(objectItems, keyOption);
+  const options = arrayKeys(objectItems, keyOption)
+    .map((tag) => ({ value: tag, label: tag, code: tag === 'PT' ? 'BR' : tag === 'EN' ? 'US' : tag }));
+
   return (
-    <div>
-      <select onChange={({ target: { value } }) => setLanguage(value)}>
-        {options.map((option) => <option key={option}>{option}</option>)}
-      </select>
+    <div className="SelectLanguage">
+      <Select
+        defaultValue={options[0]}
+        styles={{
+          menu: (provided) => ({
+            ...provided,
+            width: '200px',
+            // borderBottom: '1px dotted pink',
+            color: 'white',
+            backgroundColor: 'black',
+            padding: 20,
+            position: 'absolute',
+            margin: '0',
+          }),
+          control: (_, { selectProps: { width } }) => ({
+            width,
+            backgroundColor: 'blue',
+            display: 'flex',
+          }),
+
+          singleValue: (provided, state) => {
+            const opacity = state.isDisabled ? 0.5 : 1;
+            const transition = 'opacity 300ms';
+
+            return {
+              ...provided,
+              opacity,
+              transition,
+              color: 'pink',
+              backgroundColor: 'black',
+              width: '150px',
+              textAlign: 'center',
+            };
+          },
+        }}
+        width="200px"
+        options={options}
+        onChange={({ value }) => setLanguage(value)}
+        getOptionLabel={(e) => (
+          <div>
+            <span style={{ marginRight: 10 }}>{e.label}</span>
+            <img src={`https://flagcdn.com/${e.code.toLowerCase()}.svg`} alt="flag" width="20px" />
+          </div>
+        )}
+      />
     </div>
   );
 };
