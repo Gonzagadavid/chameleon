b467ce76b5e75777101e4ca7dcf7575acaa4f04b
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-31 15:07:40 -0300

    criado arquivo em contants exportando as rotas como constantes

---
+++ src/constants/routes.js
@@ -0,0 +1,11 @@
+export const ARTIST_DETAILS = '/artist-details';
+
+export const BIOGRAPHY = '/artist-details/biography';
+
+export const DISCOGRAPHY = '/artist-details/discography';
+
+export const ALBUM_DETAILS = '/artist-details/discography/:id';
+
+export const TRACK = '/artist-details/track/:trackName';
+
+export const VIDEOS = '/artist-details/video-music';
