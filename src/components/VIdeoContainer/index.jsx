96bbab474d8a6b228f2903b2172f00af361691f9
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-03 20:33:11 -0300

    refatorado componente VideoContainer para utilizar o componente CheckComponent

--- src/components/VIdeoContainer/index.jsx
+++ src/components/VIdeoContainer/index.jsx
@@ -1,18 +1,20 @@
 import { string } from 'prop-types';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import CheckComponent from '../CheckComponent';
 import './style.css';
 
 const VideoContainer = ({ trackName }) => {
   const musicVideos = useSelector((state) => state.musicVideos);
   const music = musicVideos.filter(({ strTrack }) => trackName === strTrack)[0];
-  if (!music) return '';
-  const embedVideo = music.strMusicVid.replace(/watch\?v=/g, 'embed/');
+  const embedVideo = music && music.strMusicVid.replace(/watch\?v=/g, 'embed/');
 
   return (
-    <div className="Video">
-      <iframe src={embedVideo} title={`video of music ${trackName}`} />
-    </div>
+    <CheckComponent condition={music}>
+      <div className="Video">
+        <iframe src={embedVideo} title={`video of music ${trackName}`} />
+      </div>
+    </CheckComponent>
   );
 };
 
