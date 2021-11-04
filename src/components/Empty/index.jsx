42faeec1d4fd26e43162179d76f2bfdc3641b29c
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-03 22:41:20 -0300

    refatorado componente Empty para receber props.children e um codição como props

--- src/components/Empty/index.jsx
+++ src/components/Empty/index.jsx
@@ -1,15 +1,24 @@
+import { bool, node } from 'prop-types';
 import React from 'react';
 import hayBall from '../../images/hayBall.gif';
 import Delayed from '../Delayed';
 import './style.css';
 
-const Empty = () => (
-  <Delayed>
-    <div>
-      <h3>Sorry, Not Found!</h3>
-      <img src={hayBall} alt="hay ball" />
-    </div>
-  </Delayed>
-);
+const Empty = ({ children, condition }) => {
+  if (condition) return children;
 
+  return (
+    <Delayed>
+      <div>
+        <h3>Sorry, Not Found!</h3>
+        <img src={hayBall} alt="hay ball" />
+      </div>
+    </Delayed>
+  );
+};
 export default Empty;
+
+Empty.propTypes = {
+  children: node.isRequired,
+  condition: bool.isRequired,
+};
