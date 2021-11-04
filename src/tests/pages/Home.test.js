8c8e97346dc47fea50bb3cb68a1b18ca15f75b73
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-04 19:10:53 -0300

    desenvolvido teste que verifica se ao renderizar o componente Home limpa o session storage

--- src/tests/pages/Home.test.js
+++ src/tests/pages/Home.test.js
@@ -1,12 +1,18 @@
-import { screen } from '@testing-library/react';
+import { cleanup, screen } from '@testing-library/react';
 import React from 'react';
+import { ARTIST_STATE } from '../../constants/storage';
 import Home from '../../pages/Home';
+import getSession from '../../services/storage/getSession';
+import setSession from '../../services/storage/setSession';
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
 import fetchMock from '../mocks/fetchMock';
 
 global.fetch = jest.fn(fetchMock);
 
-describe('verifica a renderização dos componentes', () => {
+describe('verifica a renderização o funcionamento do compoennte Home', () => {
+
+  afterEach(cleanup)
+
   it('verifica os textos estão corretos', () => {
     renderWithReduxAndRouter(<Home />);
 
@@ -24,6 +30,15 @@ describe('verifica a renderização dos componentes', () => {
 
     expect(btn).toBeInTheDocument();
     expect(btn).toContainHTML('svg')
-  
   });
+
+  it('verifica se ao rederizar o session storage é limpo', () =>{
+    setSession(ARTIST_STATE, { test: 'testing'})
+    expect(getSession(ARTIST_STATE)).toEqual({ test: 'testing'})
+    
+    renderWithReduxAndRouter(<Home />);  
+    
+    expect(getSession(ARTIST_STATE)).toBeNull()
+  })
+
 });
