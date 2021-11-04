2bbc3ad2646174da69845e8c66c494402a676e51
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-04 17:34:40 -0300

    desenvolvido teste para renderização dos elementos estaticos do componente page Home

--- src/tests/pages/Home.test.js
+++ src/tests/pages/Home.test.js
@@ -1,3 +1,4 @@
+import { screen } from '@testing-library/react';
 import React from 'react';
 import Home from '../../pages/Home';
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
@@ -7,6 +8,22 @@ global.fetch = jest.fn(fetchMock);
 
 describe('verifica a renderização dos componentes', () => {
   it('verifica os textos estão corretos', () => {
-    const { store } = renderWithReduxAndRouter(<Home />);
+    renderWithReduxAndRouter(<Home />);
+
+    const title = screen.getByRole('heading', { level: 1});
+    const serchBar = screen.getByPlaceholderText(/search/i);
+    const btn = screen.getByRole('button');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent('Chameleon');
+    expect(title).toContainHTML('img');
+    expect(title.lastChild).toHaveAttribute('alt', 'chameleon');
+
+    expect(serchBar).toBeInTheDocument();
+    expect(serchBar.tagName).toBe('INPUT');
+
+    expect(btn).toBeInTheDocument();
+    expect(btn).toContainHTML('svg')
+  
   });
 });
