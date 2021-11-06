ccc00a62d8a89d5dc80a27036a34afac5355d2a8
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-06 14:25:59 -0300

    desenvolvido testes para a renderização de imagens no componente page ArtistDetails

--- src/tests/pages/ArtistDetails.test.js
+++ src/tests/pages/ArtistDetails.test.js
@@ -4,6 +4,7 @@ import detailKeys from '../../constants/data';
 import ArtistDetails from '../../pages/ArtistDetails';
 import { DEFAULT_STATE } from '../../redux/reducers';
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
+import notFound from '../../images/notFoundImage.png';
 import { METALLICA } from '../mocks/data/ARTIST_DETAILS';
 
 const { artists: [artist] } = METALLICA;
@@ -15,7 +16,7 @@ describe('Verifica a renderização e o funcionamento do componente ArtistDetail
   });
 
   it('verifica se as informações são renderizadas corretamente', async () => {
-    await renderWithReduxAndRouter(<ArtistDetails />, { ...DEFAULT_STATE, artistDetails: artist, artistCurrent: 'metallica' });
+    await renderWithReduxAndRouter(<ArtistDetails />, { ...DEFAULT_STATE, artistDetails: artist });
     await waitFor(() => expect(screen.getAllByTestId('info')));
     const infos = screen.getAllByTestId('info');
 
@@ -24,5 +25,41 @@ describe('Verifica a renderização e o funcionamento do componente ArtistDetail
       expect(info).toHaveTextContent(artist[value]);
       expect(info.parentElement.tagName).toBe('P');
     });
+  });
+
+  it('verifica se imagem da banda é renderizada corretamente', async () => {
+    await renderWithReduxAndRouter(<ArtistDetails />, { ...DEFAULT_STATE, artistDetails: artist });
+    await waitFor(() => expect(screen.getAllByTestId('info')));
+
+    const { strArtistWideThumb: thumb } = artist;
+
+    const image = screen.getByAltText('artists');
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveProperty('src', thumb);
+  });
+
+  it('verifica se a segunda opção de imagem da banda é renderizada corretamente'
+  + ', quando não existe a primeira', async () => {
+    await renderWithReduxAndRouter(<ArtistDetails />, { ...DEFAULT_STATE, artistDetails: { ...artist, strArtistWideThumb: '' } });
+    await waitFor(() => expect(screen.getAllByTestId('info')));
+
+    const { strArtistFanart: thumb } = artist;
+
+    const image = screen.getByAltText('artists');
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveProperty('src', thumb);
+  });
+
+  it('verifica se a imagem not found é renderizada corretamente'
+  + ', quando não existe a primeira e a segunda', async () => {
+    await renderWithReduxAndRouter(<ArtistDetails />, { ...DEFAULT_STATE, artistDetails: {} });
+    await waitFor(() => expect(screen.getAllByTestId('info')));
+
+    const image = screen.getByAltText('artists');
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveProperty('src', `http://localhost/${notFound}`);
   });
 });
