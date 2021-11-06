84d8e9a62c847973a509ba1d6e9adcebbcff5c28
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-06 20:38:09 -0300

    desenvolvido testes para verificar o loading e se as requisições são feitas para albuns e para videos do artista na renderização do componente Discography

--- src/tests/mocks/fetchMock.js
+++ src/tests/mocks/fetchMock.js
@@ -1,6 +1,12 @@
 import { METALLICA, BLACK_ALIEN } from './data/ARTIST_DETAILS';
+import METALLICA_DISCOGRAPHY from './data/DISCOGRAPHY';
+import METALLICA_VIDEOS from './data/VIDEOS';
 
 const urlAtirstDetails = (artist) => `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+
+const urlDiscography = (idArtist) => `https://theaudiodb.com/api/v1/json/1/album.php?i=${idArtist}`;
+
+const urlVideos = (idArtist) => `https://theaudiodb.com/api/v1/json/1/mvid.php?i=${idArtist}`;
 
 const fetchMock = async (url) => {
   if (url === urlAtirstDetails('metallica')) {
@@ -12,6 +18,14 @@ const fetchMock = async (url) => {
   }
 
   if (url === urlAtirstDetails('nonexistent')) return { ok: true, status: 200, json: async () => ({ artists: null }) };
+
+  if (url === urlDiscography(111279)) {
+    return { ok: true, status: 200, json: async () => METALLICA_DISCOGRAPHY };
+  }
+
+  if (url === urlVideos(111279)) {
+    return { ok: true, status: 200, json: async () => METALLICA_VIDEOS };
+  }
 
   return { ok: false, status: 404 };
 };
---
+++ src/tests/pages/Discography.test.js
@@ -0,0 +1,36 @@
+import { cleanup, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import Discography from '../../pages/Discography';
+import { DEFAULT_STATE } from '../../redux/reducers';
+import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
+import { METALLICA } from '../mocks/data/ARTIST_DETAILS';
+import METALLICA_DISCOGRAPHY from '../mocks/data/DISCOGRAPHY';
+import METALLICA_VIDEOS from '../mocks/data/VIDEOS';
+import fetchMock from '../mocks/fetchMock';
+
+const { artists: [artist] } = METALLICA;
+const STATE = { ...DEFAULT_STATE, artistDetails: artist, artistCurrent: 'metallica' };
+const { album } = METALLICA_DISCOGRAPHY;
+const albuns = [...album].sort((a, b) => +a.intYearReleased - +b.intYearReleased);
+const { mvids } = METALLICA_VIDEOS;
+
+describe('Verifica a renderização e o funcionamento do componente Discography', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(fetchMock);
+  });
+
+  afterEach(cleanup);
+
+  it('verifica se o loading estiver ativo o spindle é renderizado', () => {
+    renderWithReduxAndRouter(<Discography />, { ...DEFAULT_STATE, loading: 1 });
+    expect(screen.getByAltText('loading')).toBeInTheDocument();
+  });
+
+  it('Verifica se ao rederizar a pagina a requisições são feitas e salvas no estado da aplicação', async () => {
+    const { store } = renderWithReduxAndRouter(<Discography />, STATE);
+    await waitFor(() => expect(global.fetch).toBeCalledTimes(2));
+
+    expect(store.getState().albums).toEqual(albuns);
+    expect(store.getState().musicVideos).toEqual(mvids);
+  });
+});
