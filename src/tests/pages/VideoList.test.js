832e40cd2d1a37728a6b0dcf6edefd23c9839001
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-07 12:50:03 -0300

    desenvolvido teste que verifica se ao clicar em uma track a pagina é redirecionada corretamente, no componente page VideoList

--- src/tests/pages/VideoList.test.js
+++ src/tests/pages/VideoList.test.js
@@ -1,4 +1,5 @@
 import { cleanup, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import VideoList from '../../pages/VideosList';
 import { DEFAULT_STATE } from '../../redux/reducers';
@@ -29,8 +30,24 @@ describe('verifica a renderização e o funcionamento do componente VideoList', 
     await waitFor(() => expect(global.fetch).toBeCalledTimes(1));
     await waitFor(() => expect(screen.getByText(/video music/i)).toBeInTheDocument());
 
-    mvids.forEach(({ strTrack }) => {
-      expect(screen.getAllByText(strTrack).toBeInTheDocument);
+    mvids.forEach(async ({ strTrack }) => {
+      await waitFor(() => expect(screen.getAllByText(strTrack)).toBeInTheDocument());
     });
+  });
+
+  it('verifica se ao clicar em uma track a pagina é redirecionada corretamente', async () => {
+    const { history } = renderWithReduxAndRouter(<VideoList />, STATE);
+
+    const { strTrack } = mvids[0];
+
+    await waitFor(() => expect(global.fetch).toBeCalledTimes(1));
+    await waitFor(() => expect(screen.getByText(/video music/i)).toBeInTheDocument());
+    await waitFor(() => expect(screen.getByText(strTrack)).toBeInTheDocument());
+
+    const track = screen.getByText(strTrack);
+
+    userEvent.click(track);
+
+    expect(history.location.pathname).toBe(`/artist-details/track/${strTrack}`);
   });
 });
