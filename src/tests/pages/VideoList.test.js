358a8ea36d959c49fba0486a61112f7f1d724128
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-07 12:27:31 -0300

    desenvolvido teste que verifica se todos as tracks com videos são renderizadas no componente page VideoList

--- src/tests/pages/VideoList.test.js
+++ src/tests/pages/VideoList.test.js
@@ -1,9 +1,15 @@
-import { cleanup, screen } from '@testing-library/react';
+import { cleanup, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import VideoList from '../../pages/VideosList';
 import { DEFAULT_STATE } from '../../redux/reducers';
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
+import { METALLICA } from '../mocks/data/ARTIST_DETAILS';
+import METALLICA_VIDEOS from '../mocks/data/VIDEOS';
 import fetchMock from '../mocks/fetchMock';
+
+const { artists: [artist] } = METALLICA;
+const STATE = { ...DEFAULT_STATE, artistDetails: artist, artistCurrent: 'metallica' };
+const { mvids } = METALLICA_VIDEOS;
 
 describe('verifica a renderização e o funcionamento do componente VideoList', () => {
   beforeEach(() => {
@@ -15,5 +21,16 @@ describe('verifica a renderização e o funcionamento do componente VideoList', 
   it('verifica se o loading estiver ativo o spindle é renderizado', () => {
     renderWithReduxAndRouter(<VideoList />, { ...DEFAULT_STATE, loading: 1 });
     expect(screen.getByAltText('loading')).toBeInTheDocument();
+  });
+
+  it('verifica se todos as tracks com videos são renderizadas', async () => {
+    renderWithReduxAndRouter(<VideoList />, STATE);
+
+    await waitFor(() => expect(global.fetch).toBeCalledTimes(1));
+    await waitFor(() => expect(screen.getByText(/video music/i)).toBeInTheDocument());
+
+    mvids.forEach(({ strTrack }) => {
+      expect(screen.getAllByText(strTrack).toBeInTheDocument);
+    });
   });
 });
