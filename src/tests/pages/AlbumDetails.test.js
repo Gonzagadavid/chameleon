671715167b10b0c7a71f3816df2b90c5f9e50ac9
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-08 21:00:52 -0300

    desenvolvido teste que verifica se após as requisições os dados são salvos no estado geral, no componente page AlbumsDetails

--- src/tests/pages/AlbumDetails.test.js
+++ src/tests/pages/AlbumDetails.test.js
@@ -3,10 +3,13 @@ import { DEFAULT_STATE } from '../../redux/reducers';
 import renderPathWithRedux from '../helpers/renderPathWithRedux';
 import { METALLICA } from '../mocks/data/ARTIST_DETAILS';
 import METALLICA_DISCOGRAPHY from '../mocks/data/DISCOGRAPHY';
+import METALLICA_TRACKS from '../mocks/data/TRACKS';
 import fetchMock from '../mocks/fetchMock';
 
 const { artists: [artist] } = METALLICA;
 const { album } = METALLICA_DISCOGRAPHY;
+const { track } = METALLICA_TRACKS;
+const { srtAlbum } = album;
 const STATE = {
   ...DEFAULT_STATE, artistDetails: artist, artistCurrent: 'metallica', albums: album,
 };
@@ -24,9 +27,18 @@ describe('Verifica a renderização e o funcionamento do componente page AlbumDe
     expect(screen.getByAltText('loading')).toBeInTheDocument();
   });
 
-  it('verifica se o passar o id de um album a requição é feita', async () => {
+  it('verifica se o passar o id de um album as requições são feitas', async () => {
     renderPathWithRedux('/artist-details/discography/2110232', STATE);
 
     await waitFor(() => expect(global.fetch).toBeCalledTimes(2));
+  });
+
+  it('verifica se após as requisições os dados são salvos no estado geral', async () => {
+    const { store } = renderPathWithRedux('/artist-details/discography/2110232', STATE);
+
+    await waitFor(() => expect(global.fetch).toBeCalledTimes(2));
+
+    expect(store.getState().albums).toEqual(album);
+    expect(store.getState().trackList).toEqual(track);
   });
 });
