99bde90605610aac3d35051e73bb173a988305c3
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-09 15:43:48 -0300

    desenvolvido teste que verifica se o logo da banda é renderizado, no componente Header

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,22 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+import Header from '../../components/Header';
+import { DEFAULT_STATE } from '../../redux/reducers';
+import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
+import { METALLICA } from '../mocks/data/ARTIST_DETAILS';
+
+const { artists: [artist] } = METALLICA;
+
+const STATE = { ...DEFAULT_STATE, artistDetails: artist };
+const { strArtistLogo, strArtist } = artist;
+
+describe('Verifica a renderização do componete Header', () => {
+  it('verifica se o logo da banda é renderizado', () => {
+    renderWithReduxAndRouter(<Header />, STATE);
+
+    const img = screen.getByAltText(`logo of ${strArtist}`);
+
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveProperty('src', strArtistLogo);
+  });
+});
