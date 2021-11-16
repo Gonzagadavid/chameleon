99ffd60d1098229c8f5509edfcd741a5920bc34a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-15 23:51:33 -0300

    desenvolvido teste para o custom hook useWindowDinamic

--- src/tests/hooks/hooks.test.js
+++ src/tests/hooks/hooks.test.js
@@ -1,6 +1,9 @@
-import { renderHook } from '@testing-library/react-hooks/pure';
+import { renderHook, act } from '@testing-library/react-hooks/pure';
+import matchMediaPolyfill from 'mq-polyfill';
 import useDinamicImages from '../../hooks/useDinamicImages';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 import { METALLICA } from '../mocks/data/ARTIST_DETAILS';
+import resizeTo from '../mocks/risizeTo';
 
 const { artists } = METALLICA;
 const [artist] = artists;
@@ -11,9 +14,12 @@ const artistTest = {
   strArtist, strArtistFanart, strArtistFanart2, strArtistFanart3,
 };
 
-describe('verifica o funcionamento do custom hook useDinamicImages', () => {
+matchMediaPolyfill(window);
+window.resizeTo = resizeTo;
+
+describe('verifica o funcionamento dos custom hooks ', () => {
   jest.setTimeout(16000);
-  it('verifica a renderização da imagens', async () => {
+  it('verifica o funcionamento de useDinamicImages', async () => {
     const { result, waitForValueToChange } = renderHook(() => useDinamicImages(artistTest));
 
     expect(result.current[0]).toBe(strArtistFanart);
@@ -32,5 +38,17 @@ describe('verifica o funcionamento do custom hook useDinamicImages', () => {
 
     await waitForValueToChange(() => result.current[0], { timeout: 4000 });
     expect(result.current[0]).toBe(strArtistFanart3);
+  });
+
+  it('verifica o funcionamento deuseWindowDimensions', async () => {
+    const { result } = renderHook(() => useWindowDimensions());
+
+    expect(result.current[0]).toBe(1024);
+    expect(result.current[1]).toBe(768);
+
+    act(() => window.resizeTo(360, 500));
+
+    expect(result.current[0]).toBe(360);
+    expect(result.current[1]).toBe(500);
   });
 });
---
+++ src/tests/mocks/risizeTo.js
@@ -0,0 +1,10 @@
+function resizeTo(width, height) {
+  Object.assign(this, {
+    innerWidth: width,
+    innerHeight: height,
+    outerWidth: width,
+    outerHeight: height,
+  }).dispatchEvent(new this.Event('resize'));
+}
+
+export default resizeTo;
