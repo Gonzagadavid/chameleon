1f13c0c12afd9849816346b764e0f5532dd198f2
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-12-01 23:18:42 -0300

    adicionado botão de exibição da barra de navegação em telas menores que 900px

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import LogoArtist from '../LogoArtist';
 import ContactCard from '../ContactCard';
 import './style.css';
 import nav from '../../constants/nav';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import CheckComponent from '../CheckComponent';
 
-const Nav = () => (
-  <div className="Nav">
-    <LogoArtist />
-    <ContactCard />
-    <nav>
-      <ul>
-        {nav.map(({ route, label }) => (
-          <li key={uuidv4()}><Link to={route}>{label}</Link></li>
-        ))}
-      </ul>
-    </nav>
-  </div>
-);
+const Nav = () => {
+  const [hidden, setHidden] = useState(false);
+  const [width] = useWindowDimensions();
+
+  const handleClick = () => {
+    setHidden(!hidden);
+  };
+
+  return (
+    <div className={`Nav ${hidden && 'Nav-visible'}`}>
+      <CheckComponent condition={width < 900}>
+        <button type="button" onClick={handleClick}>
+          <CheckComponent condition={hidden}>
+            <IoIosArrowBack />
+          </CheckComponent>
+          <CheckComponent condition={!hidden}>
+            <IoIosArrowForward />
+          </CheckComponent>
+        </button>
+      </CheckComponent>
+      <LogoArtist />
+      <ContactCard />
+      <nav>
+        <ul>
+          {nav.map(({ route, label }) => (
+            <li key={uuidv4()}><Link to={route}>{label}</Link></li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Nav;
