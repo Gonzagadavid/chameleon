8f822101421da5a15e0b98414d68c54f4fb0aea6
gonzagadavid <david.blesses@hotmail.com>
2022-04-01 16:58:39 -0300

    refactor: retirado linha comentada do componente page Favorites

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
 import AlbumsContainer from '../../components/AlbumsContainer';
 import Loading from '../../components/Loading';
 import TracksContainer from '../../components/TracksContainer';
