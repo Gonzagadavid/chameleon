76547e1cc215b9b84ccc1629d04ac076e664db76
gonzagadavid <david.blesses@hotmail.com>
2022-04-01 16:55:32 -0300

    refactor: refatorado arrayKeys para a operação final ser feita na linha de return

--- src/functions/arrayKeys.js
+++ src/functions/arrayKeys.js
@@ -1,7 +1,6 @@
 const arrayKeys = (object, strKey) => {
   const biographyKeys = Object.keys(object).filter((key) => key.includes(strKey) && object[key]);
-  const validKeys = biographyKeys.map((key) => key.replace(strKey, ''));
-  return validKeys;
+  return biographyKeys.map((key) => key.replace(strKey, ''));
 };
 
 export default arrayKeys;
